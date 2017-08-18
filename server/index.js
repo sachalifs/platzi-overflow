0787b130ddc2404b16398855c414af3e004c57cf
Sacha Lifszyc <sacha.lifszyc@gmail.com>
2017-08-18 20:53:57 -0300

    [Instalación y primeros pasos con Express]

--- server/index.js
+++ server/index.js
@@ -1,14 +1,9 @@
 import Debug from 'debug'
 import http from 'http'
+import app from './app'
 
 const debug = new Debug('platzi-overflow:root')
 const PORT = 3000
-
-const app = http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'})
-  res.write('Hola desde PlatziOverflow')
-  res.end()
-})
 
 app.listen(PORT, () => {
   debug(`Server running at port ${PORT}`)
